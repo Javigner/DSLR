f1556c5570168b4e25afb310c8c7557e02b4e181
Jacky VIGNERON <javigner@e1r12p18.42.fr>
2020-01-15 20:27:20 +0100

    pair plot

--- pair_plot.py
+++ pair_plot.py
@@ -1,11 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
+import seaborn as sns
 
 def main():
     dataset = pd.read_csv(sys.argv[1], index_col = "Index")
-    data = dataset[dataset.columns[6:19]]
-    cols = data.columns.values
+    dataset.dropna(inplace=True)
+    sns.pairplot(dataset, hue = 'Hogwarts House')
+    plt.show()
+    '''
+        We can see that Arithmancy and Care of Magical Creatures are homogenius beetween houses and 
+        Astronomy and Defense Against Dark Arts are identical so i will exclude them from my train_set
+        
+    '''
     
 if __name__ == "__main__":
     main()
